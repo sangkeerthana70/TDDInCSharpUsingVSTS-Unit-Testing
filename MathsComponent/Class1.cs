6bc55789d33770b5e62b3333c0a0618bbc4357fe
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-07-27 17:05:54 -0400

    TDD in c# using VSTS unit testing

---
+++ MathsComponent/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathsComponentNameSpace
+{
+    public class MathsComponent
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
--- MathsTestProject/UnitTest1.cs
+++ MathsTestProject/UnitTest1.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathsComponentNameSpace;
 
 namespace MathsTestProject
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+       public UnitTest1()
         {
+
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+
+
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            MathsComponent obj = new MathsComponent();
+            int result = obj.Add(10, 10);
+            Assert.AreEqual(20, result);
+        }
+
+        [TestMethod]
+        public void TestSubtract()
+        {
+            MathsComponent obj = new MathsComponent();
+            int result2 = obj.Subtract(100, 90);
+            Assert.AreEqual(10, result2);
         }
     }
 }
