cc0da7b2bae137b91578d29df71befa7139404e5
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-07-28 00:14:48 -0400

    Added few test cases

--- MathsComponent/Class1.cs
+++ MathsComponent/Class1.cs
@@ -17,5 +17,31 @@ namespace MathsComponentNameSpace
         {
             return num1 - num2;
         }
+
+        public bool CheckPasswdLen(string pass)
+        {
+            if(pass.Length >= 8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public int CheckGreater(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else
+            {
+                return num2;
+            }
+
+        }
     }
 }
--- MathsTestProject/UnitTest1.cs
+++ MathsTestProject/UnitTest1.cs
@@ -7,25 +7,25 @@ namespace MathsTestProject
     [TestClass]
     public class UnitTest1
     {
-       public UnitTest1()
-        {
+       //public UnitTest1()
+       // {
 
-        }
+       // }
 
-        private TestContext testContextInstance;
+       // private TestContext testContextInstance;
 
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
+       // public TestContext TestContext
+       // {
+       //     get
+       //     {
+       //         return testContextInstance;
+       //     }
 
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+       //     set
+       //     {
+       //         testContextInstance = value;
+       //     }
+       // }
 
 
 
@@ -45,5 +45,27 @@ namespace MathsTestProject
             int result2 = obj.Subtract(100, 90);
             Assert.AreEqual(10, result2);
         }
+
+        [TestMethod]
+        public void TestCheckPasswdLength()
+        {
+            MathsComponent obj = new MathsComponent();
+            bool result = obj.CheckPasswdLen("Steakhouse");
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestCheckGreater()
+        {
+            MathsComponent great = new MathsComponent();
+            int final = great.CheckGreater(-5, 10);
+            Assert.AreEqual(10, final);
+        }
+
+
+
+
+
+
     }
 }
